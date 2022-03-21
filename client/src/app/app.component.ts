371a506a6916e5adc7b3356887df392cc4200a93
spjulien3 <spjulien3@gmail.com>
2022-03-21 04:16:56 -0400

    User registration

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+import { setTheme } from 'ngx-bootstrap/utils';
+import { User } from './models/User';
+import { AccountService } from './services/account.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,12 +14,19 @@ export class AppComponent implements OnInit{
   title = 'Bug Byte';
   users: any;
 
-  constructor(private http : HttpClient)
+  constructor(private http : HttpClient, private accountService : AccountService)
   {
-
+    setTheme('bs4');
+    this.setCurrentUser();
   }
+
+  setCurrentUser() {
+    const user : User =  JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
+  }
+
   ngOnInit(){
-    this.http.get('https://localhost:5001/api/users').subscribe(
+    this.http.get('https://localhost:5003/api/users').subscribe(
       response => {
         this.users = response;
         console.log(this.users);
