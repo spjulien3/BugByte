394936fcd3b56d58e2fa33c7d8ab7ab8e58b169c
Sam Julien <spjulien3@gmail.com>
2022-05-01 20:05:15 -0400

    Project Crud Completed

--- API/Controllers/ProjectController.cs
+++ API/Controllers/ProjectController.cs
@@ -51,5 +51,45 @@ namespace API.Controllers
 
             return await GetProjectByProjectId((int)newProject.Id);
         }
+
+
+        [HttpPut("/update-project")]
+        public async Task<ActionResult<AppProject>> UpdateProject(UpdateProjectDto request)
+        {
+
+            var project = await _context.Projects
+                .Where(t => t.Id == request.Id)
+                .FirstOrDefaultAsync();
+            if (project == null) return BadRequest();
+
+            var milestone = await _context.Milestones
+                .Where(u => u.Id == request.milestoneId)
+                .FirstOrDefaultAsync();
+            if (milestone == null) return BadRequest();
+
+
+            project.Title = request.Title;
+            project.Description = request.Description;
+            project.MilestoneId = request.milestoneId;
+            project.Milestone = milestone;
+            
+
+
+            await _context.SaveChangesAsync();
+            return Ok(project);
+
+        }
+
+        [HttpDelete("/delete-project/{id}")]
+        public async Task<ActionResult<AppProject>> DeleteTicket(int id)
+        {
+            var project = await _context.Projects.Where(t => t.Id == id).FirstOrDefaultAsync();
+            if (project == null) return BadRequest();
+
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
+            return Ok(project);
+
+        }
     }
 }
---
+++ API/DTOs/UpdateProjectDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTOs
+{
+    public class UpdateProjectDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int? milestoneId { get; set; }
+    }
+}
